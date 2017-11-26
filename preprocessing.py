3b3fbf7344fa43e4e3679651cec268a8284f17fc
heartcored98 <33013451+heartcored98@users.noreply.github.com>
2017-11-26 18:18:53 +0900

    add comment and refactored

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,12 @@
+import json
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import copy
-color = sns.color_palette()
-
 import gensim
-from gensim.models.word2vec import Word2Vec # the word2vec model gensim class
-from nltk.tokenize import TweetTokenizer # a tweet tokenizer from nltk.
+from gensim.models.word2vec import Word2Vec  # the word2vec model gensim class
+from nltk.tokenize import TweetTokenizer  # a tweet tokenizer from nltk.
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
-import json
-from pprint import pprint
 
 tokenizer = TweetTokenizer()
 LabeledSentence = gensim.models.doc2vec.LabeledSentence  # we'll talk about this down below
@@ -21,6 +16,7 @@ W2V_MODEL_NAME = 'tweet_w2v_model.json'
 FILENAME_A = 'data/semeval_train_A.txt'
 FILENAME_B = 'data/semeval_train_B.txt'
 FILENAME_C = 'data/semeval_train_C.txt'
+BASE_PATH = './W2V_MODEL/'
 
 INDEX_A = ['SentimentText', 'Sentiment']
 INDEX_B = ['ttext', 'topic', 'sentiment']
@@ -36,8 +32,8 @@ def ingest(filename, index_names):
     print("=====================================================================")
     return df
 
-def postprocess(data, n=-1):
 
+def preprocess(data, n=-1):
     def tokenize(tweet):
         tweet = tweet['SentimentText']
         try:
@@ -62,10 +58,11 @@ def postprocess(data, n=-1):
 
 def labelizeTweets(tweets, label_type):
     labelized = []
-    for i,v in tqdm(enumerate(tweets)):
-        label = '%s_%s'%(label_type,i)
+    for i, v in tqdm(enumerate(tweets)):
+        label = '%s_%s' % (label_type, i)
         labelized.append(LabeledSentence(v, [label]))
     return labelized
+
 
 def train_w2v(train_data):
     tweet_w2v = Word2Vec(size=W2V_N_DIM, min_count=10)
@@ -73,54 +70,75 @@ def train_w2v(train_data):
     tweet_w2v.train([x.words for x in tqdm(train_data)], total_examples=tweet_w2v.corpus_count, epochs=tweet_w2v.iter)
     return tweet_w2v
 
+
 def save_w2v(model, filename):
-    model.save(filename)
+    model.save(BASE_PATH + filename)
+
 
 def load_w2v(filename):
-    model = gensim.models.Word2Vec.load(filename)
+    model = gensim.models.Word2Vec.load(BASE_PATH + filename)
     return model
+
 
 def get_vocab_list(model):
     corpus = list(model.wv.vocab.keys())
     return corpus
+
 
 def vocab2vec(model, vocabs):
     vocab_vector_dict = dict()
     for vocab in tqdm(vocabs):
         vector = model[vocab]
         vocab_vector_dict[vocab] = vector.tolist()
+
+    print("Vocabulary Set converted to Vector [total: {} words]".format(len(vocab_vector_dict)))
     return vocab_vector_dict
 
+
 def save_json(data, filename):
-    with open(filename, 'w') as outfile:
+    with open(BASE_PATH + filename, 'w') as outfile:
         json.dump(data, outfile)
 
 
+def load_json(filename):
+    data = None
+    with open(BASE_PATH + filename, 'r') as json_data:
+        data = json.load(json_data)
+    return data
 
 
 if __name__ == '__main__':
-
-
+    # Loading txt file and preprocessing
     data = ingest(filename=FILENAME_A, index_names=INDEX_A)
-    data = postprocess(data)
+    data = preprocess(data)
 
+    # Devide train set and test set but now set test size to 0
     x_train, x_test, y_train, y_test = train_test_split(np.array(data.tokens),
                                                         np.array(data.Sentiment),
                                                         test_size=0.0)
-
+    # Tag training data and test data
     x_train = labelizeTweets(x_train, 'TRAIN')
     x_test = labelizeTweets(x_test, 'TEST')
 
-    # model = train_w2v(train_data=x_train)
-    # save_w2v(model=model, filename=W2V_MODEL_NAME)
+    # train and save word2vec model
+    model = train_w2v(train_data=x_train)
+    save_w2v(model=model, filename=W2V_MODEL_NAME)
+
+    # show how to load word2vec model
     w2v_model = load_w2v(filename=W2V_MODEL_NAME)
+
+    # make vocabulary list from model
     corpus = get_vocab_list(w2v_model)
+
+    # convert each vocabulary word to voctor using model
     vector_dict = vocab2vec(w2v_model, corpus)
-    print("Vocabulary Set converted to Vector [total: {} words]".format(len(vector_dict)))
+
+    # save vocab-vector dictionary into json format
     save_json(vector_dict, "vocab_vector.json")
-    # voca = tweet_w2v.sorted_vocab
 
-
-
-    # a = 'dec 21st 2012 will be know not as the end'
-    # print(tokenize(a))
+    # show how to load vocab-vector dictionary
+    # vector size is 150 dimension now but it could be changed
+    vocab_vector = load_json("vocab_vector.json")
+    vocab_list = list(vocab_vector.keys())
+    print(vocab_vector['you'])
+    print(vocab_list[:10])
