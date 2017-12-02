37e37a836a1c6722c6dd67d2e7c3cadae00a5f5e
heartcored98 <33013451+heartcored98@users.noreply.github.com>
2017-12-02 20:17:59 +0900

    conflict update2

--- spellchecker.py
+++ spellchecker.py
@@ -1,39 +1,44 @@
 import re, collections
 
-def words(text):
-    return re.findall('[a-z]+', text.lower())
 
-def train(features):
-    model = collections.defaultdict(lambda: 1)
-    for f in features:
-        model[f] += 1
-    return model
+class SpellChecker:
+    def __init__(self):
+        self.NWORDS = None
+        self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        self.model = None
 
-filename = './big.txt'
-with open(filename, 'r') as inputfile:
-    dataset = inputfile.read()
+    def words(self, text):
+        return re.findall('[a-z]+', text.lower())
 
-NWORDS = train(words(dataset))
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    def train(self, features):
+        model = collections.defaultdict(lambda: 1)
+        for f in features:
+            model[f] += 1
+        self.NWORDS = model
 
-def edits1(word):
-    s = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-    deletes    = [a + b[1:] for a, b in s if b]
-    transposes = [a + b[1] + b[0] + b[2:] for a, b in s if len(b)>1]
-    replaces   = [a + c + b[1:] for a, b in s for c in alphabet if b]
-    inserts    = [a + c + b     for a, b in s for c in alphabet]
-    return set(deletes + transposes + replaces + inserts)
+    def edits1(self, word):
+        s = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes = [a + b[1:] for a, b in s if b]
+        transposes = [a + b[1] + b[0] + b[2:] for a, b in s if len(b) > 1]
+        replaces = [a + c + b[1:] for a, b in s for c in self.alphabet if b]
+        inserts = [a + c + b for a, b in s for c in self.alphabet]
+        return set(deletes + transposes + replaces + inserts)
 
-def known_edits2(word):
-    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+    def known_edits2(self, word):
+        return set(e2 for e1 in self.edits1(word) for e2 in self.edits1(e1) if e2 in self.NWORDS)
 
-def known(words):
-    return set(w for w in words if w in NWORDS)
+    def known(self, words):
+        return set(w for w in words if w in self.NWORDS)
 
-def correct(word):
-    candidates = known([word]) or known(edits1(word)) or    known_edits2(word) or [word]
-    return max(candidates, key=NWORDS.get)
+    def correct(self, word):
+        candidates = self.known([word]) or self.known(self.edits1(word)) or self.known_edits2(word) or [word]
+        return max(candidates, key=self.NWORDS.get)
+
 
 if __name__ == '__main__':
-    a = correct("banaa")
+    sc = SpellChecker()
+    data = ["banana", "apple", "fuck"]
+    sc.train(data)
+
+    a = sc.correct("banaa")
     print(a)
